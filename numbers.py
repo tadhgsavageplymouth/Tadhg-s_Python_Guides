026d74d011ea6552ebbf3e950100bd41b1f15c78
tadhgsavageplymouth <tadhg.savage@students.plymouth.ac.uk>
2024-06-15 16:19:18 +0100

    Additional comments for clarity

--- numbers.py
+++ numbers.py
@@ -1,23 +1,31 @@
+### Addition and printing of the addition vaiable can be done as demostrated below
 addition = 2 + 2
 print (addition)
 
+### Subtraction and printing of the subtraction vaiable can be done as demostrated below
 subtraction = 3 - 2
 print (subtraction)
 
+### Multiplication and printing of the multiplication vaiable can be done as demostrated below
 multiplication = 3 * 3
 print (multiplication)
 
+### Division and printing of the division vaiable can be done as demostrated below
 division = 3 / 2
 print (division)
 
+### Exponent expression given below
 exponenet = 3 ** 3
 print (exponenet)
 
+### Concatenating print of multiple variables 
 print (addition, subtraction, multiplication, division, exponenet)
 
+### Order of operations are handled by python
 operation_order = 4 + (2*2) ** 2
 print (operation_order)
 
+### Long numbers can be expressed by utilising the "_" python expresses without _
 universe_age = 1_000_000_000
 print (f'{universe_age} years, is the age of the universe')
 
