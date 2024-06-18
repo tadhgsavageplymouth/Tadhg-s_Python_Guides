56d276e22c067fcf69df23c4df382c4cff9850ce
tadhgsavageplymouth <tadhg.savage@students.plymouth.ac.uk>
2024-06-18 12:17:57 +0100

    Additions to numerical_lists.py

--- Guides/numerical_lists.py
+++ Guides/numerical_lists.py
@@ -12,3 +12,19 @@ print()
 even_numbers = list(range(0, 12, 2))
 print (even_numbers)
 
+# Lets create a list of the first 10 square numbers
+# First create an empty list
+squares = []
+# For loop to create the numbers 1-10
+for value in range(1, 11):
+    # Now squaring our 1-10 values
+    square = value ** 2
+    # Appending squared values into list
+    squares.append(square)
+# Coming out of loop and printing our list
+print (squares)
+
+
+
+
+
