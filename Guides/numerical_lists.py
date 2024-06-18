f6c9adfc25de94d901a9f7caddb12f80d0f8ce27
tadhgsavageplymouth <tadhg.savage@students.plymouth.ac.uk>
2024-06-18 11:56:23 +0100

    Addition of numerical_list.py

---
+++ Guides/numerical_lists.py
@@ -0,0 +1,14 @@
+# Generating a series of numbers using loop
+for value in range(2, 10):
+    print (value)
+print()
+
+# Creating a list of numbers with delimiter
+numbers = list(range(1, 11, 2))
+print (numbers)
+print()
+
+# Creating a list of even numbers
+even_numbers = list(range(0, 12, 2))
+print (even_numbers)
+
